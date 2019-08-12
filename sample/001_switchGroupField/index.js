0dc29df61e8f138513db66576f59c797458e86be
zennosuke <zen.e.0719@gmail.com>
2019-08-12 20:22:45 +0900

    add customize-manifest.json

--- sample/001_switchGroupField/index.js
+++ sample/001_switchGroupField/index.js
@@ -22,13 +22,13 @@
         kintone.app.record.setGroupFieldOpen('group_failure', true);
         break;
       case '要望':
-        kintone.app.record.setGroupFieldOpen('group_demand', false);
+        kintone.app.record.setGroupFieldOpen('group_demand', true);
         break;
       case '質問':
-        kintone.app.record.setGroupFieldOpen('group_question', false);
+        kintone.app.record.setGroupFieldOpen('group_question', true);
         break;
       case 'その他':
-        kintone.app.record.setGroupFieldOpen('group_other', false);
+        kintone.app.record.setGroupFieldOpen('group_other', true);
         break;
     }
   });
