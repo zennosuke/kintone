4bf75c5bd386df3eaf4f95c629d8ad6be6f78f04
zennosuke <zen.e.0719@gmail.com>
2019-08-12 20:11:07 +0900

    initial commit

---
+++ sample/001_switchGroupField/README.md
@@ -0,0 +1,2 @@
+# 回答の条件によってグループフィールドの開閉を切り替える
+https://developer.cybozu.io/hc/ja/articles/207377396
---
+++ sample/001_switchGroupField/index.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  var events = [
+    'app.record.detail.show',
+    'app.record.create.show',
+    'app.record.edit.show',
+    'app.record.create.change.radioButton',
+    'app.record.edit.change.radioButton'];
+
+  kintone.events.on(events, function(e) {
+    var record = e.record;
+    var radioButtonValue = record.radioButton.value;
+
+    kintone.app.record.setGroupFieldOpen('group_failure', false);
+    kintone.app.record.setGroupFieldOpen('group_demand', false);
+    kintone.app.record.setGroupFieldOpen('group_question', false);
+    kintone.app.record.setGroupFieldOpen('group_other', false);
+
+    switch (radioButtonValue) {
+      case '不具合':
+        kintone.app.record.setGroupFieldOpen('group_failure', true);
+        break;
+      case '要望':
+        kintone.app.record.setGroupFieldOpen('group_demand', false);
+        break;
+      case '質問':
+        kintone.app.record.setGroupFieldOpen('group_question', false);
+        break;
+      case 'その他':
+        kintone.app.record.setGroupFieldOpen('group_other', false);
+        break;
+    }
+  });
+})();
